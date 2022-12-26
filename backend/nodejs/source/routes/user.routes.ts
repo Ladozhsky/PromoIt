e6baa47a9e83749423bb0eb6d956e37c092d7ed0
Vlad <vlad_aleks93@mail.ru>
2022-12-26 18:08:46 +0200

    server run, added packeages

---
+++ backend/nodejs/source/routes/user.routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+//import controller from '../controllers/user.controller';
+
+const router = express.Router();
+
+//router.get('/:id', controller.getById);
+
+export default { router };
