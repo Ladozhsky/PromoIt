f5fc666f4d9c29c8492d85ff5aa005a1eca48fed
deadhecore <dm.podporin@gmail.com>
2022-12-27 15:45:20 +0200

    connection to SQL + changing IdentityUser to ApplicationIdentityUser with migration and DB update

---
+++ frontend/frontend/Areas/Identity/ApplicationIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace frontend.Areas.Identity
+{
+    public class ApplicationIdentityUser : IdentityUser
+    {
+        public long ApplicationID { get; set; }
+    }
+}
---
+++ frontend/frontend/Data/Migrations/20221227133235_User Changed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace frontend.Data.Migrations
+{
+    public partial class UserChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ApplicationID",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationID",
+                table: "AspNetUsers");
+        }
+    }
+}
