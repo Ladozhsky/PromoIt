37395a9199c600c12ea958c7afb1af22945226d3
Vlad <vlad_aleks93@mail.ru>
2022-12-30 22:49:00 +0200

    crud for users done, also added dto for users

---
+++ backend/asp/PromoIt/PromoItAPI/Models/Tweet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromoItAPI.Models
+{
+    public partial class Tweet
+    {
+        public int TweetId { get; set; }
+        public int UserId { get; set; }
+    }
+}
