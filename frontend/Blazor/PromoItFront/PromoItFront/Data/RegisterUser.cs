23716a035de55e4e5a36cb2a6ff9df707d7ff541
Vlad <vlad_aleks93@mail.ru>
2023-01-04 19:01:46 +0200

    register works

---
+++ frontend/Blazor/PromoItFront/PromoItFront/Data/RegisterUser.cs
@@ -0,0 +1,16 @@
+﻿namespace PromoItFront.Data
+{
+    public partial class RegisterUser
+    {
+
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Address { get; set; }
+        public int TelNumber { get; set; }
+        public int RoleId { get; set; }
+        public int? CompanyId { get; set; }
+
+    }
+}
