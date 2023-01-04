c970c76bace48fc3d44244f333da9efa395c8166
Vlad <vlad_aleks93@mail.ru>
2023-01-04 23:18:48 +0200

    telnumber formatted in register form

--- backend/asp/PromoIt/PromoItAPI/Models/User.cs
+++ backend/asp/PromoIt/PromoItAPI/Models/User.cs
@@ -18,7 +18,7 @@ namespace PromoItAPI.Models
         public string Password { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Address { get; set; }
-        public int TelNumber { get; set; }
+        public string TelNumber { get; set; }
         public int RoleId { get; set; }
         public int? CompanyId { get; set; }
 
--- frontend/Blazor/PromoItFront/PromoItFront/Data/RegisterUser.cs
+++ frontend/Blazor/PromoItFront/PromoItFront/Data/RegisterUser.cs
@@ -8,7 +8,7 @@
         public string Password { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Address { get; set; }
-        public int TelNumber { get; set; }
+        public string TelNumber { get; set; }
         public int RoleId { get; set; }
         public int? CompanyId { get; set; }
 
