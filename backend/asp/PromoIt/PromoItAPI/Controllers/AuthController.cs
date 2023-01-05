ea115d81e4ab681ee7e06263168ed08837fc2343
Vlad <vlad_aleks93@mail.ru>
2023-01-05 19:49:08 +0200

    refactor auth. register with hash and salt added, begin login

---
+++ backend/asp/PromoIt/PromoItAPI/Controllers/AuthController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PromoItAPI.Models;
+using PromoItAPI.ModelsDto;
+using System.Security.Cryptography;
+
+namespace PromoItAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly promoitContext _context;
+
+        public AuthController(promoitContext context)
+        {
+            _context = context;
+        }
+
+        public static User user = new User();
+        public static LoginDto userLogin = new LoginDto();
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(UserDto request, string username)
+        {
+            var existingUser = _context.Users.FirstOrDefault(u => u.UserName == username);
+
+            if (existingUser == null)
+            {
+                CreatePasswordHash(request.Password, out string passwordHash, out string passwordSalt);
+
+                user.UserName = request.UserName;
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.Email = request.Email;
+                user.Address = request.Address;
+                user.TelNumber = request.TelNumber;
+                user.RoleId = request.RoleId;
+                user.CompanyId = request.CompanyId;
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest("User with this username is already exists");
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginDto request)
+        {
+            var users = _context.Users;
+            var existingUser = _context.Users.FirstOrDefault(u => u.UserName == request.userName);
+
+            if (existingUser == null)
+            {
+                return BadRequest("User not found");
+            }
+            return Ok("Token");
+        }
+
+        private void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = Convert.ToBase64String(hmac.Key);
+                passwordHash = Convert.ToBase64String(hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)));
+            }
+        }
+
+        private static LoginDto UserDTOtoLoginDTO(UserDto login) =>
+        new LoginDto
+        {
+            userName = login.UserName,
+            password = login.Password
+        };
+    }
+}
--- backend/asp/PromoIt/PromoItAPI/Controllers/UsersController.cs
+++ backend/asp/PromoIt/PromoItAPI/Controllers/UsersController.cs
@@ -1,196 +1,196 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using PromoItAPI.Models;
-using PromoItAPI.ModelsDto;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using PromoItAPI.Models;
+//using PromoItAPI.ModelsDto;
 
-namespace PromoItAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
-    {
-        private readonly promoitContext _context;
+//namespace PromoItAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class UsersController : ControllerBase
+//    {
+//        private readonly promoitContext _context;
 
-        public UsersController(promoitContext context)
-        {
-            _context = context;
-        }
+//        public UsersController(promoitContext context)
+//        {
+//            _context = context;
+//        }
 
-        //GET: api/Users
-       //[HttpGet]
-       // public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-       // {
-       //     return await _context.Users.ToListAsync();
-       // }
+//        //GET: api/Users
+//       //[HttpGet]
+//       // public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+//       // {
+//       //     return await _context.Users.ToListAsync();
+//       // }
 
-		[HttpGet]
-		public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
-		{
-			return await _context.Users.Select(u => UserToDTO(u)).ToListAsync();
-		}
+//		[HttpGet]
+//		public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+//		{
+//			return await _context.Users.Select(u => UserToDTO(u)).ToListAsync();
+//		}
 
 
-		// GET: api/Users/5
-		//[HttpGet("{id}")]
-  //      public async Task<ActionResult<User>> GetUser(int id)
-  //      {
-  //          var user = await _context.Users.FindAsync(id);
+//		// GET: api/Users/5
+//		//[HttpGet("{id}")]
+//  //      public async Task<ActionResult<User>> GetUser(int id)
+//  //      {
+//  //          var user = await _context.Users.FindAsync(id);
 
-  //          if (user == null)
-  //          {
-  //              return NotFound();
-  //          }
+//  //          if (user == null)
+//  //          {
+//  //              return NotFound();
+//  //          }
 
-  //          return user;
-  //      }
+//  //          return user;
+//  //      }
 
-		[HttpGet("{id}")]
-		public async Task<ActionResult<UserDto>> GetUser(int id)
-		{
-			var user = await _context.Users.FindAsync(id);
+//		[HttpGet("{id}")]
+//		public async Task<ActionResult<UserDto>> GetUser(int id)
+//		{
+//			var user = await _context.Users.FindAsync(id);
 
-			if (user == null)
-			{
-				return NotFound();
-			}
+//			if (user == null)
+//			{
+//				return NotFound();
+//			}
 
-			return UserToDTO(user);
-		}
+//			return UserToDTO(user);
+//		}
 
-		// PUT: api/Users/5
-		//[HttpPut("{id}")]
-		//public async Task<IActionResult> PutUser(int id, User user)
-		//{
-		//    if (id != user.UserId)
-		//    {
-		//        return BadRequest();
-		//    }
+//		// PUT: api/Users/5
+//		//[HttpPut("{id}")]
+//		//public async Task<IActionResult> PutUser(int id, User user)
+//		//{
+//		//    if (id != user.UserId)
+//		//    {
+//		//        return BadRequest();
+//		//    }
 
-		//    _context.Entry(user).State = EntityState.Modified;
+//		//    _context.Entry(user).State = EntityState.Modified;
 
-		//    try
-		//    {
-		//        await _context.SaveChangesAsync();
-		//    }
-		//    catch (DbUpdateConcurrencyException)
-		//    {
-		//        if (!UserExists(id))
-		//        {
-		//            return NotFound();
-		//        }
-		//        else
-		//        {
-		//            throw;
-		//        }
-		//    }
+//		//    try
+//		//    {
+//		//        await _context.SaveChangesAsync();
+//		//    }
+//		//    catch (DbUpdateConcurrencyException)
+//		//    {
+//		//        if (!UserExists(id))
+//		//        {
+//		//            return NotFound();
+//		//        }
+//		//        else
+//		//        {
+//		//            throw;
+//		//        }
+//		//    }
 
-		//    return NoContent();
-		//}
+//		//    return NoContent();
+//		//}
 
-		[HttpPut("{id}")]
-		public async Task<IActionResult> PutUser(int id, UserDto userDto)
-		{
-			if (id != userDto.UserId)
-			{
-				return BadRequest();
-			}
+//		[HttpPut("{id}")]
+//		public async Task<IActionResult> PutUser(int id, UserDto userDto)
+//		{
+//			if (id != userDto.UserId)
+//			{
+//				return BadRequest();
+//			}
 
-            var user = await _context.Users.FindAsync(id);
-			if (user == null)
-            {
-                return NotFound();
-            }
+//            var user = await _context.Users.FindAsync(id);
+//			if (user == null)
+//            {
+//                return NotFound();
+//            }
 
-            user.UserName = userDto.UserName;
-            user.Password = userDto.Password;
-            user.Email= userDto.Email;
-            user.Address = userDto.Address;
-            user.TelNumber = userDto.TelNumber;
-            user.RoleId = userDto.RoleId;
-            user.CompanyId = userDto.CompanyId;
+//            user.UserName = userDto.UserName;
+//            user.Password = userDto.Password;
+//            user.Email= userDto.Email;
+//            user.Address = userDto.Address;
+//            user.TelNumber = userDto.TelNumber;
+//            user.RoleId = userDto.RoleId;
+//            user.CompanyId = userDto.CompanyId;
 
-			try
-			{
-				await _context.SaveChangesAsync();
-			}
-			catch (DbUpdateConcurrencyException) when (!UserExists(id))
-			{
-                return NotFound();
-			}
+//			try
+//			{
+//				await _context.SaveChangesAsync();
+//			}
+//			catch (DbUpdateConcurrencyException) when (!UserExists(id))
+//			{
+//                return NotFound();
+//			}
 
-			return NoContent();
-		}
+//			return NoContent();
+//		}
 
-		// POST: api/Users
-		//[HttpPost]
-		//public async Task<ActionResult<User>> PostUser(User user)
-		//{
-		//    _context.Users.Add(user);
-		//    await _context.SaveChangesAsync();
+//		// POST: api/Users
+//		//[HttpPost]
+//		//public async Task<ActionResult<User>> PostUser(User user)
+//		//{
+//		//    _context.Users.Add(user);
+//		//    await _context.SaveChangesAsync();
 
-		//    return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
-		//}
+//		//    return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
+//		//}
 
-		[HttpPost]
-		public async Task<ActionResult<UserDto>> PostUser(UserDto userDto)
-		{
-            var user = new User
-            {
-                UserId= userDto.UserId,
-                UserName = userDto.UserName,
-                Password = userDto.Password,
-                Email = userDto.Email,
-                Address = userDto.Address,
-                TelNumber = userDto.TelNumber,
-                RoleId = userDto.RoleId,
-                CompanyId = userDto.CompanyId
-            };
+//		[HttpPost]
+//		public async Task<ActionResult<UserDto>> PostUser(UserDto userDto)
+//		{
+//            var user = new User
+//            {
+//                UserId= userDto.UserId,
+//                UserName = userDto.UserName,
+//                Password = userDto.Password,
+//                Email = userDto.Email,
+//                Address = userDto.Address,
+//                TelNumber = userDto.TelNumber,
+//                RoleId = userDto.RoleId,
+//                CompanyId = userDto.CompanyId
+//            };
 
-			_context.Users.Add(user);
-			await _context.SaveChangesAsync();
+//			_context.Users.Add(user);
+//			await _context.SaveChangesAsync();
 
-			return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, UserToDTO(user));
-		}
+//			return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, UserToDTO(user));
+//		}
 
-		// DELETE: api/Users/5
+//		// DELETE: api/Users/5
 
-		[HttpDelete("{id}")]
-		public async Task<IActionResult> DeleteUser(int id)
-		{
-			var user = await _context.Users.FindAsync(id);
-			if (user == null)
-			{
-				return NotFound();
-			}
+//		[HttpDelete("{id}")]
+//		public async Task<IActionResult> DeleteUser(int id)
+//		{
+//			var user = await _context.Users.FindAsync(id);
+//			if (user == null)
+//			{
+//				return NotFound();
+//			}
 
-			_context.Users.Remove(user);
-			await _context.SaveChangesAsync();
+//			_context.Users.Remove(user);
+//			await _context.SaveChangesAsync();
 
-			return NoContent();
-		}
+//			return NoContent();
+//		}
 
-		private bool UserExists(int id)
-        {
-            return _context.Users.Any(e => e.UserId == id);
-        }
+//		private bool UserExists(int id)
+//        {
+//            return _context.Users.Any(e => e.UserId == id);
+//        }
 
-		private static UserDto UserToDTO(User user) =>
-	    new UserDto
-	   {
-		   UserId = user.UserId,
-		   UserName = user.UserName,
-           Password = user.Password,
-           Email = user.Email,
-		   Address = user.Address,
-           TelNumber= user.TelNumber,
-           RoleId= user.RoleId,
-           CompanyId = user.CompanyId
-	   };
-	}
-}
+//		private static UserDto UserToDTO(User user) =>
+//	    new UserDto
+//	   {
+//		   UserId = user.UserId,
+//		   UserName = user.UserName,
+//           Password = user.Password,
+//           Email = user.Email,
+//		   Address = user.Address,
+//           TelNumber= user.TelNumber,
+//           RoleId= user.RoleId,
+//           CompanyId = user.CompanyId
+//	   };
+//	}
+//}
