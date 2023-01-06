8756964ed5a4733bba1d781e4b10e5bdd05a5fad
Vlad <vlad_aleks93@mail.ru>
2023-01-06 16:02:30 +0200

    register on front works with new api

--- frontend/Blazor/PromoItFront/PromoItFront/Data/RegisterUser.cs
+++ frontend/Blazor/PromoItFront/PromoItFront/Data/RegisterUser.cs
@@ -3,14 +3,14 @@
     public partial class RegisterUser
     {
 
-        public int UserId { get; set; }
+        //public int UserId { get; set; }
         public string UserName { get; set; } = null!;
         public string Password { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Address { get; set; }
         public string TelNumber { get; set; }
         public int RoleId { get; set; }
-        public int? CompanyId { get; set; }
+        //public int? CompanyId { get; set; }
 
     }
 }
