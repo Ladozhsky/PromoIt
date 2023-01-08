f40c916ac8fbb2c2aecb8228c10238cd1bb81f0f
Vlad <vlad_aleks93@mail.ru>
2023-01-08 19:24:40 +0200

    create new API, autorize works for get campaigns

---
+++ backend/api/APIPRromoIt/APIPRromoIt/Controllers/CampaignsController.cs
@@ -0,0 +1,39 @@
+﻿using APIPRromoIt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIPRromoIt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampaignsController : ControllerBase
+    {
+        private readonly promoitContext _context;
+
+        public CampaignsController(promoitContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<CampaignDto>>> GetCampaigns()
+        {
+            return await _context.Campaigns.Select(c => CampaignToTDO(c)).ToListAsync();
+        }
+
+        private static CampaignDto CampaignToTDO(Campaign campaign) =>
+        new CampaignDto
+       {
+           CampaignId = campaign.CampaignId,
+           CampaignName = campaign.CampaignName,
+           Hashtag = campaign.Hashtag,
+           Description = campaign.Description,
+           UserId = campaign.UserId,
+           CreateDate = campaign.CreateDate,
+           CompanyId = campaign.CompanyId
+       };
+    }
+}
---
+++ backend/api/APIPRromoIt/APIPRromoIt/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIPRromoIt.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Campaigns = new HashSet<Campaign>();
+            Orders = new HashSet<Order>();
+            UserBalances = new HashSet<UserBalance>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Address { get; set; }
+        public string TelNumber { get; set; } = null!;
+        public int RoleId { get; set; }
+        public int? CompanyId { get; set; }
+
+        public virtual Role Role { get; set; } = null!;
+        public virtual ICollection<Campaign> Campaigns { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<UserBalance> UserBalances { get; set; }
+    }
+}
