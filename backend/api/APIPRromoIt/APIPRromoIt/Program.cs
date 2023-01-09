d604162b58168c9012c74c2d30f96d1244c93a8b
Vlad <vlad_aleks93@mail.ru>
2023-01-09 19:48:24 +0200

    auth0 works! get campaigns and create campaiin with authentication

--- backend/api/APIPRromoIt/APIPRromoIt/Program.cs
+++ backend/api/APIPRromoIt/APIPRromoIt/Program.cs
@@ -1,14 +1,43 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+       {
+          {
+              new OpenApiSecurityScheme
+                  {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                     }
+                   },
+                       new string[]{}
+           }
+      });
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddAuthentication(options =>
 {
@@ -17,7 +46,7 @@ builder.Services.AddAuthentication(options =>
 }).AddJwtBearer(options =>
 {
     options.Authority = "https://dev-h265dhdwa46lpcmn.us.auth0.com/";
-    options.Audience = "http://promoit/api";
+    options.Audience = "dgwNLi968wdHkRnMzI6oLo2wo1KoDxve";
 });
 
 builder.Services.AddDbContext<APIPRromoIt.Models.promoitContext>(
--- frontend/Blazor/PromoItBlazorServer/PromoItBlazorServer/PromoItBlazorServer/Program.cs
+++ frontend/Blazor/PromoItBlazorServer/PromoItBlazorServer/PromoItBlazorServer/Program.cs
@@ -1,7 +1,10 @@
 using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using PromoItBlazorServer.Data;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +12,57 @@ builder.Services
     .AddAuth0WebAppAuthentication(options => {
         options.Domain = builder.Configuration["Auth0:Domain"];
         options.ClientId = builder.Configuration["Auth0:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+        options.OpenIdConnectEvents = new OpenIdConnectEvents
+        {
+            OnTokenValidated = (context) =>
+            {
+                var token = context.SecurityToken.RawHeader + "." +
+                    context.SecurityToken.RawPayload + "." + context.SecurityToken.RawSignature;
+                var claims = new List<Claim>
+                    {
+                        new Claim("jwt_token", token)
+                    };
+                var appIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                context.Principal.AddIdentity(appIdentity);
+                context.Response.Cookies.Append("auth_token", token, new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict
+                });
+
+                return Task.CompletedTask;
+            },
+            OnTicketReceived = notification =>
+            {
+                Console.WriteLine("Authentication ticket received.");
+                return Task.FromResult(true);
+            },
+
+            OnAuthorizationCodeReceived = notification =>
+            {
+                Console.WriteLine("Authorization code received");
+                return Task.FromResult(true);
+            },
+            OnUserInformationReceived = notification =>
+            {
+                Console.WriteLine("Token validation received");
+                return Task.FromResult(true);
+            }
+        };
+
+    }).WithAccessToken(options =>
+    {
+        options.Audience = builder.Configuration["Auth0:Audience"];
+        options.UseRefreshTokens = true;
     });
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
