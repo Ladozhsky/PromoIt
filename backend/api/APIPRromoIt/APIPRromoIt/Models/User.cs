1bccd60342365f05801524d9947fbdece111c9f6
Vlad <vlad_aleks93@mail.ru>
2023-01-10 22:13:05 +0200

    before scaffold

--- backend/api/APIPRromoIt/APIPRromoIt/Models/User.cs
+++ backend/api/APIPRromoIt/APIPRromoIt/Models/User.cs
@@ -7,22 +7,22 @@ namespace APIPRromoIt.Models
     {
         public User()
         {
-            Campaigns = new HashSet<Campaign>();
+            //Campaigns = new HashSet<Campaign>();
             Orders = new HashSet<Order>();
             UserBalances = new HashSet<UserBalance>();
         }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public string UserName { get; set; } = null!;
-        public string PasswordHash { get; set; } = null!;
+        //public string PasswordHash { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Address { get; set; }
         public string TelNumber { get; set; } = null!;
-        public int RoleId { get; set; }
+        public int Role { get; set; }
         public int? CompanyId { get; set; }
 
-        public virtual Role Role { get; set; } = null!;
-        public virtual ICollection<Campaign> Campaigns { get; set; }
+        //public virtual Role Role { get; set; } = null!;
+        //public virtual ICollection<Campaign> Campaigns { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
         public virtual ICollection<UserBalance> UserBalances { get; set; }
     }
---
+++ backend/api/APIPRromoIt/APIPRromoIt/ModelsDto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace APIPRromoIt.ModelsDto
+{
+    public partial class UserDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Address { get; set; }
+        public string TelNumber { get; set; } = null!;
+        public int Role { get; set; }
+        public int? CompanyId { get; set; }
+    }
+}
