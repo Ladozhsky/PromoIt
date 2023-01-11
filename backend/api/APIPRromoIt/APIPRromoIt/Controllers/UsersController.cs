8d4f9938489907896dfa7a396a04f7706905ac8f
Vlad <vlad_aleks93@mail.ru>
2023-01-11 20:12:59 +0200

    changed types

--- backend/api/APIPRromoIt/APIPRromoIt/Controllers/UsersController.cs
+++ backend/api/APIPRromoIt/APIPRromoIt/Controllers/UsersController.cs
@@ -21,15 +21,15 @@ namespace APIPRromoIt.Controllers
 
         // Add user
         [HttpPost]
-        //[Authorize]
+        [Authorize]
         public async Task<ActionResult<User>> PostUser(UserDto userDto)
         {
             var identity = HttpContext.User.Identity as ClaimsIdentity;
-            var userId = identity?.FindFirst("user_id")?.Value;
-            var role = identity?.FindFirst("https://promoit.co.il/claims/role")?.Value;
-            var email = identity?.FindFirst("https://promoit.co.il/claims/email")?.Value;
+            string userId = identity?.FindFirst("user_id")?.Value;
+            string role = identity?.FindFirst("https://promoit.co.il/claims/role")?.Value;
+            string email = identity?.FindFirst("https://promoit.co.il/claims/email")?.Value;
 
-            var user = new User
+            User user = new User
             {
                 UserId = userId,
                 UserName = userDto.UserName,
@@ -44,30 +44,6 @@ namespace APIPRromoIt.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(user);
-            //return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, UserToTDO(user));
         }
-
-        //public async Task<ActionResult<UserDto>> GetUser(string id)
-        //{
-        //    var user = await _context.Users.FindAsync(id);
-
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return UserToTDO(user);
-        //}
-
-        //private static UserDto UserToTDO(User user) =>
-        //new UserDto
-        //{
-        //    UserId = user.UserId,
-        //    UserName = user.UserName,
-        //    Email = user.Email,
-        //    Address = user.Address,
-        //    TelNumber = user.TelNumber,
-        //    Role = user.Role,
-        //    CompanyId = user.CompanyId
-        //};
     }
 }
