c3d13bcde329a58488d7cd1d2d4bd86cf65f1854
Vlad <vlad_aleks93@mail.ru>
2023-01-12 15:21:56 +0200

    works forms create user and create company

---
+++ backend/api/APIPRromoIt/APIPRromoIt/Controllers/CompaniesController.cs
@@ -0,0 +1,58 @@
+﻿using APIPRromoIt.Models;
+using APIPRromoIt.ModelsDto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace APIPRromoIt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly promoitContext _context;
+
+        public CompaniesController(promoitContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<CompanyDto>>> GetCompanies()
+        {
+            return await _context.Companies.Select(c => CompanyToTDO(c)).ToListAsync();
+        }
+
+        // Add user
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<Company>> PostCompany(CompanyDto companyDto)
+        {
+            Company company = new Company
+            {
+                CompanyId = companyDto.CompanyId,
+                CompanyName = companyDto.CompanyName,
+                Site = companyDto.Site,
+                Email = companyDto.Email,
+                CompanyType = companyDto.CompanyType
+            };
+
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return Ok(company);
+        }
+        private static CompanyDto CompanyToTDO(Company company) =>
+        new CompanyDto
+        {
+            CompanyId = company.CompanyId,
+            CompanyName = company.CompanyName,
+            Site = company.Site,
+            Email = company.Email,
+            CompanyType = company.CompanyType
+        };
+    }
+}
---
+++ backend/api/APIPRromoIt/APIPRromoIt/ModelsDto/CompanyDto.cs
@@ -0,0 +1,13 @@
+﻿using APIPRromoIt.Models;
+
+namespace APIPRromoIt.ModelsDto
+{
+    public class CompanyDto
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; } = null!;
+        public string Site { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string CompanyType { get; set; }
+    }
+}
