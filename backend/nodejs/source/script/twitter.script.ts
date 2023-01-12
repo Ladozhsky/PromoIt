819964d80284c974281c2c5077e2bc4fe0898d5e
deadhecore <dm.podporin@gmail.com>
2023-01-12 19:47:43 +0200

    twitter parsing done

--- backend/nodejs/source/script/twitter.script.ts
+++ backend/nodejs/source/script/twitter.script.ts
@@ -1,6 +1,10 @@
 import addRetweets from "../controllers/twitter.controllet";
 import { ListCreation } from "../services/list-creation.services";
+import * as cron from 'node-cron'
 
 const retweetList : ListCreation = new ListCreation
 
-addRetweets(retweetList.createListOfRetweets())
+cron.schedule('18 18 * * *', () => {
+    addRetweets(retweetList.createListOfRetweets())
+})
+
