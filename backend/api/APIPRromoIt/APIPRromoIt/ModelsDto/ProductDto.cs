ff33ad5261eba76448ad8225590784395cd013f1
Vlad <vlad_aleks93@mail.ru>
2023-01-13 20:45:54 +0200

    add product and request for it

---
+++ backend/api/APIPRromoIt/APIPRromoIt/ModelsDto/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using APIPRromoIt.Models;
+
+namespace APIPRromoIt.ModelsDto
+{
+    public partial class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int Price { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ frontend/Blazor/PromoItBlazorServer/PromoItBlazorServer/PromoItBlazorServer/Data/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace PromoItBlazorServer.Data
+{
+    public partial class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int Price { get; set; }
+        public string? Image { get; set; }
+    }
+}
