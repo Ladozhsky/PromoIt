ffe340f37e5ca44442fd1df5efac7b8ee0ad5a78
Vlad <vlad_aleks93@mail.ru>
2023-01-14 16:56:00 +0200

    page for each campaign created and redirect works

---
+++ backend/api/APIPRromoIt/APIPRromoIt/Controllers/OrdersController.cs
@@ -0,0 +1,57 @@
+﻿using APIPRromoIt.Models;
+using APIPRromoIt.ModelsDto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace APIPRromoIt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly promoitContext _context;
+
+        public OrdersController(promoitContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<OrderDto>> PostCampaign(OrderDto orderDto)
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            string userId = identity?.FindFirst("user_id")?.Value;
+
+            var companyId = await _context.Users.Where(x => x.UserId == userId)
+                                            .Select(x => x.CompanyId)
+                                            .FirstOrDefaultAsync();
+
+            Order order = new Order
+            {
+                OrderId = orderDto.OrderId,
+                CampaignId = orderDto.CampaignId,
+                CompanyId = orderDto.CompanyId,
+                UserId = userId,
+            };
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            OrderToProduct orderToProduct = new OrderToProduct
+            {
+                OrderId = order.OrderId,
+                ProductId = orderDto.ProductId,
+                Amount = orderDto.Amount,
+            };
+
+            _context.OrderToProducts.Add(orderToProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok(orderDto);
+        }
+    }
+}
---
+++ backend/api/APIPRromoIt/APIPRromoIt/ModelsDto/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using APIPRromoIt.Models;
+
+namespace APIPRromoIt.ModelsDto
+{
+    public partial class OrderDto
+    {
+        public int OrderId { get; set; }
+        public int CampaignId { get; set; }
+        public int CompanyId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+    }
+}
