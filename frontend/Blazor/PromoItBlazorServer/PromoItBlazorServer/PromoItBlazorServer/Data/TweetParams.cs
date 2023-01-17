ae052b45948790614a06392324a3611582e63c39
Vlad <vlad_aleks93@mail.ru>
2023-01-17 18:50:57 +0200

    tweetparams added

---
+++ frontend/Blazor/PromoItBlazorServer/PromoItBlazorServer/PromoItBlazorServer/Data/TweetParams.cs
@@ -0,0 +1,10 @@
+﻿namespace PromoItBlazorServer.Data
+{
+    public class TweetParams
+    {
+        public string twitter_user_id { get; set; }
+        public string campaign { get; set; }
+        public string company { get; set; }
+        public string product { get; set; }
+    }
+}
