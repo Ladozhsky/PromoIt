e3cdc3ebbac0b32cf7b7f773a8e10f1bf440a01f
Vlad <vlad_aleks93@mail.ru>
2023-01-19 17:55:09 +0200

    page all users added

--- backend/api/APIPRromoIt/APIPRromoIt/ModelsDto/UserDto.cs
+++ backend/api/APIPRromoIt/APIPRromoIt/ModelsDto/UserDto.cs
@@ -1,4 +1,6 @@
-﻿namespace APIPRromoIt.ModelsDto
+﻿using System.Reflection.Metadata;
+
+namespace APIPRromoIt.ModelsDto
 {
     public partial class UserDto
     {
@@ -7,5 +9,16 @@
         public string TelNumber { get; set; } = null!;
         public string Role { get; set; }
         public int? CompanyId { get; set; }
-    }
+
+	}
+
+	public partial class UserForAdmin
+	{
+		public string UserName { get; set; } = null!;
+		public string? Address { get; set; }
+		public string TelNumber { get; set; } = null!;
+		public string Role { get; set; }
+		public string? CompanyName { get; set; }
+		public string Status { get; set; }
+	}
 }
