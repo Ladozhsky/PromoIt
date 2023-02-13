bf2fd5eeda906f04cb8eb5b66754c9e3127f9e77
Vlad <vlad_aleks93@mail.ru>
2023-02-13 11:21:21 +0200

    for merge

---
+++ backend/nodejs/source/database/student.db.ts
@@ -0,0 +1,4 @@
+import { IStudent } from "./models/student.model";
+
+export const StudentDB: IStudent[] = [{name: "Vlad", country: "Russia", age: 29}, 
+{name: "Tanya", country: "Israel", age: 29}];
